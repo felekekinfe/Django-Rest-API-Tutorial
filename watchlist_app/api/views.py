94e2758bdbd4f20bc7ccedc458cc4115b1f1d10d
felekekinfe <felekekinfe@gmail.com>
2024-02-28 08:20:48 -0500

    class based view

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -22,16 +22,29 @@ class MovieListAV(APIView):
             return Response(serializer.errors)
 
 class MovieDetailAV(APIView):
-    def get(self,request):
+    def get(self,request,pk):
         try:
             movie=Movie.objects.get(pk=pk)
         except Movie.DoesNotExist:
             return Response({'errors':'Movie not found'})
         
         serializer=MovieSerializer(movie)
-        return Response(serializer)
-    def put(self, request):
-        movi
+        return Response(serializer.data)
+    def put(self, request,pk):
+        movie=Movie.objects.get(pk=pk)
+        serializer=MovieSerializer(movie, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self,request,pk):
+        movie=Movie.objects.get(pk=pk)
+        movie.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
 # @api_view(['GET','POST'])
 # def movie_list(request):
 
