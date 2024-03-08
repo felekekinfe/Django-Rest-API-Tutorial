20c06639aff2e26a9f09e85e51e24c1367e6b476
felekekinfe <felekekinfe@gmail.com>
2024-03-07 23:36:45 -0500

    commit

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -23,3 +23,10 @@ class WatchList(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    rating=models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description=models.CharField(max_length=200,null=True)
+    watchlist=models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='reviews')
+    active=models.BooleanField(default=True)
+    created
