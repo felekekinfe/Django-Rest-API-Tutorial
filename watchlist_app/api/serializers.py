79c75abc789a7c7eafe28d968f9d49067fea5eb7
felekekinfe <felekekinfe@gmail.com>
2024-03-08 00:15:29 -0500

    commit

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from watchlist_app.models import WatchList,StreamPlatform,Review
 
 
-class WatchListSerializer(serializers.ModelSerializer):
+class ReviewSerializer(serializers.ModelSerializer):
+    
 
+    class Meta:
+        model=Review
+        fields='__all__'
+
+class WatchListSerializer(serializers.ModelSerializer):
+    reviews=ReviewSerializer(many=True, read_only=True)
     
     class Meta:
         model=WatchList
@@ -25,12 +32,7 @@ class StreamPlatformSerializer(serializers.ModelSerializer):
     class Meta:
         model=StreamPlatform
         fields='__all__'
-class ReviewSerializer(serializers.ModelSerializer):
-    reviews=WatchListSerializer(many=True, read_only=True)
 
-    class Meta:
-        model=Review
-        fields='__all__'
 
     # def validate(self, data):
     #     if data['name']==data['description']:
