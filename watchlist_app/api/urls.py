bf7de63bb05e7364331dcae25d32dd68255c4b67
felekekinfe <felekekinfe@gmail.com>
2024-03-12 09:18:28 -0400

    commit

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import StreamPlatformAV,StreamPlatformDetail,WatchListAV,WatchDetailAV,ReviewList,ReviewDetails
+from .views import ReviewCreate,StreamPlatformAV,StreamPlatformDetail,WatchListAV,WatchDetailAV,ReviewList,ReviewDetails
 
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
      path('stream/<int:pk>',StreamPlatformDetail.as_view(),name='stream_detail'),
      # path('review',ReviewList.as_view(), name='review-list'),
      # path('review/<int:pk>',ReviewDetails.as_view(), name='review-details'),
+      path('stream/<int:pk>/review-create',ReviewCreate.as_view(), name='review-create'),
      path('stream/<int:pk>/review',ReviewList.as_view(), name='review-list'),
      path('stream/review/<int:pk>',ReviewDetails.as_view(), name='review-details')
 
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -6,10 +6,24 @@ from watchlist_app.models import WatchList,StreamPlatform,Review
 from django.shortcuts import render
 from .serializers import WatchListSerializer,ReviewSerializer, StreamPlatformSerializer
 
+class ReviewCreate(generics.CreateAPIView):
+    serializer_class=ReviewSerializer
+
+    def perform_create(self, serializer):
+        pk=self.kwargs.get('pk')
+        movie=WatchList.objects.get(pk=pk)
+
+
+        serializer.save(self,watchlist=movie)
 
 class ReviewList(generics.ListCreateAPIView):
-    queryset=Review.objects.all()
+    # queryset=Review.objects.all()
     serializer_class=ReviewSerializer
+
+    def get_queryset(self):
+        pk=self.kwargs['pk']
+        return Review.objects.filter(watchlist=pk)
+    
 
 class ReviewDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset=Review.objects.all()
